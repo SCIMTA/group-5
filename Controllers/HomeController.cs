73e94811b9f6e76a7c43c9008ab917301691e25d
ThuyTran <tranthuthuy1002@gmail.com>
2021-04-19 21:21:03 +0700

    sua text

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace taka.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "hoang ham";
 
             return View();
         }
