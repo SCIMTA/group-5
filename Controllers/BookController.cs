eb2a348cd5ba0c46651b7b400d09ab97660f4eaa
ThuyTran <tranthuthuy1002@gmail.com>
2021-04-26 16:30:31 +0700

    thêm view book/ delete+details

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -33,13 +33,20 @@ namespace taka.Controllers
         public ActionResult Add(Book book)
         {
             bookUtils.InsertBook(book.Title, (decimal)book.Price, (int)book.Page, (int)book.Year, (int)book.Quantity, book.Description,
-                                ViewBag.idCategory, (int)book.idType, (int)book.idPublisher, (int)book.idLanguage, (int)book.idAuthor);
+                                (int)book.idCategory, (int)book.idType, (int)book.idPublisher, (int)book.idLanguage, (int)book.idAuthor);
 
             return RedirectToAction("Index");
         }
 
         public ActionResult Edit(int ID)
         {
+            Taka db = bookUtils.getDatabase();
+            ViewBag.listCategories = db.Categories.ToList(); ;
+            ViewBag.listPublishers = db.Publishers.ToList();
+            ViewBag.listLanguages = db.Languages.ToList();
+            ViewBag.listAuthors = db.Authors.ToList();
+            ViewBag.listTypes = db.Types.ToList();
+
             Book book = bookUtils.FindBookById(ID);
             return View(book);
         }
@@ -48,6 +55,27 @@ namespace taka.Controllers
         public ActionResult Edit(Book book)
         {
             bookUtils.UpdateBook(book);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int ID)
+        {
+            Taka db = bookUtils.getDatabase();
+            Book book = bookUtils.FindBookById(ID);
+            return View(book);
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            Taka db = bookUtils.getDatabase();
+            Book book = bookUtils.FindBookById(ID);
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Book book)
+        {
+            bookUtils.DeleteBook(book);
             return RedirectToAction("Index");
         }
     }
