3cff037f006aa0a7e96e4831fe69468455610f02
hoangnh <hoangnh5901@outlook.com>
2021-05-06 22:18:17 +0700

    add controller

---
+++ WEB/Controllers/AdminController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using taka.Models.DatabaseInteractive;
+
+namespace taka.Controllers
+{
+    public class AdminController : Controller
+    {
+        TakaDB dB = new TakaDB();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit(int id = -1)
+        {
+            ViewBag.listCategories = dB.GetCategories();
+            ViewBag.listAuthors = dB.GetAuthors();
+            ViewBag.listPublishers = dB.GetPublishers();
+            ViewBag.listLanguages = dB.GetLanguages();
+            try
+            {
+                if (id == -1)
+                    throw new Exception("Not found");
+                var item = dB.GetBookDetail(id);
+                return View(item);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult EditBook(int MA_DAU_SACH,
+            HttpPostedFileBase BIA_SACH,
+            string TEN_DAU_SACH,
+            int MA_THE_LOAI,
+            int MA_NHA_XUAT_BAN,
+            int MA_TAC_GIA,
+            int GIA_TRI,
+            int SO_TRANG,
+            int TONG_SO_LUONG,
+            int SO_LUONG_CON_LAI,
+            string MO_TA_SACH)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id = -1)
+        {
+            try
+            {
+                if (id == -1)
+                    throw new Exception("Not found");
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -55,7 +55,7 @@ namespace taka.Controllers
         [HttpPost]
         public ActionResult Register(string phone, string password, string email, string gender, string fullname, string birthday, string callbackUrl)
         {
-            User user = dB.Register(phone, password,email,gender,fullname,birthday);
+            User user = dB.Register(phone, password, email, gender, fullname, birthday);
 
             if (user != null)
             {
@@ -87,54 +87,6 @@ namespace taka.Controllers
             }
 
         }
-
-        public ActionResult Edit(int id = -1)
-        {
-            ViewBag.listCategories = dB.GetCategories();
-            ViewBag.listAuthors = dB.GetAuthors();
-            ViewBag.listPublishers = dB.GetPublishers();
-            ViewBag.listLanguages = dB.GetLanguages();
-            return Detail(id);
-        }
-
-        [HttpPost]
-        public ActionResult EditBook(int MA_DAU_SACH,
-            HttpPostedFileBase BIA_SACH,
-            string TEN_DAU_SACH,
-            int MA_THE_LOAI,
-            int MA_NHA_XUAT_BAN,
-            int MA_TAC_GIA,
-            int GIA_TRI,
-            int SO_TRANG,
-            int TONG_SO_LUONG,
-            int SO_LUONG_CON_LAI,
-            string MO_TA_SACH)
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Delete(int id = -1)
-        {
-            try
-            {
-                if (id == -1)
-                    throw new Exception("Not found");
-                return RedirectToAction("Index", "Home");
-            }
-            catch (Exception e)
-            {
-                return RedirectToAction("Error", "Home");
-            }
-
-        }
-
-        public ActionResult Add()
-        {
-
-            return View();
-        }
-
         public ActionResult Error()
         {
             return View();
